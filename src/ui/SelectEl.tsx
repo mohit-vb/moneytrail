c50a5e5b0e7a53fa82a03cf76c2272a72301dd68
mohit-vb <mohitvbjadhav@gmail.com>
2026-09-23 22:53:43 +0530

    feat(style) : style select UI component separatly for reusability

---
+++ src/ui/SelectEl.tsx
@@ -0,0 +1,31 @@
+type SelectElProps = React.ComponentProps<"select">;
+
+export default function SelectEl({ children, ...props }: SelectElProps) {
+  return (
+    <div className="relative inline-block">
+      <select
+        className="appearance-none w-max border border-accent/40 py-1 px-4 rounded-full flex items-center gap-2 cursor-pointer bg-ink"
+        {...props}
+      >
+        {children}
+      </select>
+
+      {/* Custom Dropdown Icon Container */}
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-gray-500">
+        <svg
+          className="h-4 w-4"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
