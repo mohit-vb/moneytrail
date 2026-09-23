1e79191cc2aedd052323d8eef1803586186786d0
mohit-vb <mohitvbjadhav@gmail.com>
2026-09-23 19:12:29 +0530

    chore(imports) : centeralized components for single line import

---
+++ src/features/transactions/components/index.ts
@@ -0,0 +1,13 @@
+import TransactionTable from "./TransactionTable";
+import TransactionRow from "./TransactionRow";
+import TransactionTypeFilter from "./TransactionTypeFilter";
+import TransactionDateFilter from "./TransactionDateFilter";
+import ActiveFilterChips from "./ActiveFilterChips";
+
+export {
+  TransactionTable,
+  TransactionRow,
+  TransactionTypeFilter,
+  TransactionDateFilter,
+  ActiveFilterChips,
+};
